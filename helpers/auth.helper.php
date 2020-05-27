3b2572dcffd65c29e35e90df410351719eb88d59
Sasha Lujan <lujnannatasha1989@gmail.com>
2020-05-27 18:36:14 -0300

    agregue la carpeta helpers

---
+++ helpers/auth.helper.php
@@ -0,0 +1,21 @@
+<?php
+
+class AuthHelper {
+
+    //verifica que haya un usuario logueado
+    static public function userLogged() {
+        session_start();
+        if(isset($_SESSION['IS_LOGGED'])) {
+            return true;
+        }
+        return false;
+    }
+
+    static public function checkLogged() {
+        session_start(); 
+        if (!isset($_SESSION['IS_LOGGED'])) {
+            header('Location: ' . BASE_URL . 'home');
+            die();
+        }
+    }
+}
