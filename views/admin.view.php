b63f7e0d4a9290b0c916dda095ae41b86a4a4b04
Sasha Lujan <lujnannatasha1989@gmail.com>
2020-05-27 17:37:49 -0300

    actualice los archivos

---
+++ views/admin.view.php
@@ -0,0 +1,60 @@
+<?php
+
+require_once 'libs/Smarty.class.php';
+
+class AdminView{
+
+    private $smarty;
+    
+    public function __construct(){
+        $this->smarty = new Smarty();
+    }
+
+    public function welcome($admin) {
+        $this->smarty->assign('nombre_admin', $admin);
+        $this->smarty->display('templates/welcome.tpl');
+    }
+
+    //muestra un formulario para elegir si quiere modificar computadoras o marcas
+    public function chooseTask() {
+        $this->smarty->display('templates/chooseTask.tpl');
+    }
+
+    //
+    public function showComputers($computadoras) {
+        $this->smarty->assign('listaComp', $computadoras);
+        $this->smarty->display('templates/showComputers.tpl');
+    }
+
+    public function showMarks($marcas) {
+        $this->smarty->assign('listaMarca', $marcas);
+        $this->smarty->display('templates/showMarks.tpl');
+    }    
+
+    public function formComputerAdd($marcas, $error = null) {
+        $this->smarty->assign('listaMarca', $marcas);
+        $this->smarty->assign('error', $error);
+        $this->smarty->display('templates/formComputerAdd.tpl');
+    }
+
+    public function formMarkAdd($error = null) {
+        $this->smarty->assign('error', $error);
+        $this->smarty->display('templates/formMarkAdd.tpl');
+    }
+
+    public function showFormEditComputer($computadora, $marca, $error = null){
+        $this->smarty->assign('computadora', $computadora);
+        $this->smarty->assign('listaMarca', $marca);
+        $this->smarty->assign('error', $error);
+        $this->smarty->display('templates/showFormEditComputer.tpl');
+    }
+    public function showFormEditMarca($marca, $error = null){
+        $this->smarty->assign('marca', $marca);
+        $this->smarty->assign('error', $error);
+        $this->smarty->display('templates/showFormEditMarca.tpl');
+    }
+    public function showError($msg){
+        $this->smarty->assign('mensaje', $msg);
+        $this->smarty->display('templates/error.tpl');
+    }
+}
---
+++ views/public.view.php
@@ -0,0 +1,59 @@
+<?php
+
+require_once 'libs/Smarty.class.php';
+
+class PublicView{
+    private $smarty;
+
+    public function __construct(){
+        $this->smarty = new Smarty();
+        
+    }
+
+    public function showHome($admin, $error = null){
+        $this->smarty->assign('isAdmin', $admin);
+        $this->smarty->assign('error', $error);
+        $this->smarty->display('templates/showHome.tpl');
+    }
+
+    public function showError($msg) {
+        $this->smarty->assign('mensaje', $msg);
+        $this->smarty->display('templates/error.tpl');
+    }
+
+    public function showComputers($computadoras, $admin, $error = null) {
+        $this->smarty->assign('listaComp', $computadoras);
+        $this->smarty->assign('isAdmin', $admin);
+        $this->smarty->assign('error', $error);
+        $this->smarty->display('templates/showPlayers.tpl');
+    }
+
+    public function showComputer($computadora, $computadoras, $admin, $error = null) {
+        $this->smarty->assign('datosComp', $computadora);
+        $this->smarty->assign('listaComp', $computadoras);
+        $this->smarty->assign('isAdmin', $admin);
+        $this->smarty->assign('error', $error);
+        $this->smarty->display('templates/showComputer.tpl');
+    }
+
+    public function showComputerMark($computadora, $computadoras, $admin, $error = null) {
+        $this->smarty->assign('datosComp', $computadora);
+        $this->smarty->assign('listaComp', $computadoras);
+        $this->smarty->assign('isAdmin', $admin);
+        $this->smarty->assign('error', $error);
+        $this->smarty->display('templates/showComputerMark.tpl');
+    }
+    public function showMarks($marcas, $admin, $error = null) {
+        $this->smarty->assign('listaMarca', $marcas);
+        $this->smarty->assign('isAdmin', $admin);
+        $this->smarty->assign('error', $error);
+        $this->smarty->display('templates/showMarks.tpl');
+    }    
+
+    public function ComputersByMark($computadoraPorMarca, $admin, $error = null) {
+       $this->smarty->assign('computadoraPorMarca', $computadoraPorMarca);
+       $this->smarty->assign('isAdmin', $admin);
+       $this->smarty->assign('error', $error);
+       $this->smarty->display('templates/ComputersByMark.tpl');
+    }
+}
