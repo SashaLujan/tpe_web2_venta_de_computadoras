19077286f834e7c71fea012ebf198ff3e279653a
Sasha Lujan <lujnannatasha1989@gmail.com>
2020-06-30 17:21:17 -0300

    agregue funcion para eliminar y agregar comentario

--- api/comments-api.controller.php
+++ api/comments-api.controller.php
@@ -3,24 +3,64 @@
 require_once 'models/comentarios.model.php';
 require_once 'api/api.view.php';
 
-class CommentsApiController {
+class CommentsApiController
+{
     private $model;
     private $view;
+    private $data;
 
-    public function __construct() {
+    public function __construct()
+    {
         $this->model = new ComentariosModel();
         $this->view = new APIView();
+        $this->data = file_get_contents("php://input");  //Lee el input ingresado
     }
 
-    public function getComments($params) {
+    public function getComments($params)
+    {
         $id_computadora = $params[':ID'];   //Obtengo el id de una computadora del arreglo asociativo $params
         $comentarios = $this->model->getAll($id_computadora);
-        if(!empty($comentarios)) {
-            $this->view->response($comentarios, 200); 
+        if (!empty($comentarios)) {
+            $this->view->response($comentarios, 200);
         } else {
             $this->view->response("No hay comentarios sobre esta computadora", 404);
         }
+    }
 
-        
+    //funcion para eliminar un comentario
+    public function deleteComment($params = [])
+    {
+        $id_comentario = $params[':ID'];
+        $comentario = $this->model->get($id_comentario);
+        if (!empty($comentario)) {
+            $this->model->delete($id_comentario);
+            $this->view->response("El comentario se elimino", 200);
+        } else {
+            $this->view->response("El comentario no existe ", 404);
+        }
+    }
+
+    public function getData()
+    {
+        return json_decode($this->data);
+    }
+
+    public function addComment()
+    {
+        //Devuelve el objeto JSON enviado por POST ($body es un objeto JSON)
+        $body = $this->getData();
+
+        $comentario = $body->comentario;
+        $usuario = $body->usuario;
+        $fecha = $body->fecha;
+        $puntaje = $body->puntaje;
+        $id_computadora = $body->id_computadora;
+        $id_comentario = $this->model->insert($comentario, $usuario, $fecha, $puntaje, $id_computadora);
+
+        if ($id_comentario) {
+            $this->view->response("Se guardo el comentario", 200);
+        } else {
+            $this->view->response("El comentario no se pudo agregar", 500);
+        }
     }
 }
