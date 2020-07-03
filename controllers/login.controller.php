826d3be85915884fa9fb8269685029cbb9aed6fb
Sasha Lujan <lujnannatasha1989@gmail.com>
2020-07-03 10:27:03 -0300

    agregue funcion de eliminar, mostrar y editar usuario

--- controllers/login.controller.php
+++ controllers/login.controller.php
@@ -53,8 +53,9 @@ class LoginController
     }
 
     //agregar usuario
-    public function addUser() {
-        if(empty($_POST['nombre']) || empty($_POST['email']) || empty($_POST['contraseña']) || empty($_POST['confirmarContraseña'])) {
+    public function addUser()
+    {
+        if (empty($_POST['nombre']) || empty($_POST['email']) || empty($_POST['contraseña']) || empty($_POST['confirmarContraseña'])) {
             $this->viewPublic->formCheck("Todos los datos son obligatorios");
         } else {
             $nombre = $_POST['nombre'];
@@ -63,29 +64,49 @@ class LoginController
             $repitPassword = $_POST['confirmarContraseña'];
             $tipo = "usuario";
             $user = $this->modelLogin->getAdmin($username);
-            if($user) {
-                $this->viewPublic->formCheck("El usuario ya estaba registrado");
+            if ($user) {
+                $this->viewPublic->formCheck("El usuario ya esta registrado");
             } else {
-                if($password != $repitPassword) {
-                    $this->viewPublic->formCheck("Las contraseñas no coinciden"); 
+                if ($password != $repitPassword) {
+                    $this->viewPublic->formCheck("Las contraseñas no coinciden");
                 } else {
                     $passwordCifrado = password_hash($password, PASSWORD_DEFAULT);
                     $this->modelLogin->insert($nombre, $username, $passwordCifrado, $tipo);
-                    if(session_status() != PHP_SESSION_ACTIVE){
+                    if (session_status() != PHP_SESSION_ACTIVE) {
                         session_start(); //Abro la sesion
                     }
                     $_SESSION['IS_LOGGED'] = true;
                     $_SESSION['nombre_usuario'] = $nombre;  //Guardo el nombre del usuario
                     $_SESSION['TIPO'] = $tipo;
-                    header('Location: ' .BASE_URL. 'home');
+                    header('Location: ' . BASE_URL . 'home');
                 }
             }
         }
     }
 
     //verifica que tipo de usuario es
-    public function formCheckIn() {
-        $this->viewPublic->formCheck(); 
-     }
- 
+    public function formCheckIn()
+    {
+        $this->viewPublic->formCheck();
+    }
+
+    //muestra todo los usuarios registrados
+    public function showUsers()
+    {
+        $usuarios = $this->modelLogin->get();
+        $tipos = $this->modelLogin->types();
+        $this->view->showUsers($usuarios, $tipos);
+    }
+
+    //elimina un usuario
+    public function deleteUser()
+    {
+        $this->modelLogin->delete($id_usuario);
+        header('Location: ' . BASE_URL . 'listar_usuarios');
+    }
+
+    //modifica los datos de un usuario
+    public function editUser()
+    {
+    }
 }
