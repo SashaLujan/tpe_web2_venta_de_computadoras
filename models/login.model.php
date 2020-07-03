1f686533613abe21cd4e0ee91d5cd407fe99effd
Sasha Lujan <lujnannatasha1989@gmail.com>
2020-07-03 17:08:19 -0300

    actualice las funciones del login

--- models/login.model.php
+++ models/login.model.php
@@ -30,6 +30,54 @@ class LoginModel
          // 2. enviamos la consulta (3 pasos)
          $sentencia = $db->prepare("INSERT INTO administradores (nombre, nombre_usuario, contraseña, tipo) VALUES (?, ?, ?, ?)"); // prepara la consulta
          return $sentencia->execute([$nombre, $nombre_usuario, $contraseña, $tipo]);
+    }
 
+    //devuelve todos los de usuarios y admin que hay registrados
+    public function get(){
+        // 1. abro la conexión con MySQL 
+        $db = $this->createConection();
+        // 2. enviamos la consulta (3 pasos)
+        $sentencia = $db->prepare("SELECT * FROM usuarios"); // prepara la consulta
+        $sentencia->execute(); // ejecuta
+        $usuarios = $sentencia->fetchAll(PDO::FETCH_OBJ); // obtiene la respuesta
+        return $usuarios;
+    }
+
+    //devuelve los tipos de usuarios
+    public function types(){
+        // 1. abro la conexión con MySQL 
+        $db = $this->createConection();
+        // 2. enviamos la consulta (3 pasos)
+        $sentencia = $db->prepare("SELECT DISTINCT tipo FROM usuarios"); // prepara la consulta
+        $sentencia->execute(); // ejecuta
+        $tipos = $sentencia->fetchAll(PDO::FETCH_OBJ); // obtiene la respuesta
+        return $tipos;
+    }
+
+    public function delete ($id_usuario){
+        // 1. abro la conexión con MySQL 
+        $db = $this->createConection();
+        // 2. enviamos la consulta (3 pasos)
+        $sentencia = $db->prepare("DELETE FROM usuarios WHERE id_usuario = ?"); // prepara la consulta
+        $sentencia->execute([$id_usuario]); // ejecuta
+    }
+
+    //devuelve un usuario pasado por parametro
+    public function getId($id_usuario){
+        // 1. abro la conexión con MySQL 
+        $db = $this->createConection();
+        // 2. enviamos la consulta (3 pasos)
+        $sentencia = $db->prepare("SELECT * FROM usuarios WHERE id_usuario = ?"); // prepara la consulta
+        $sentencia->execute([$id_usuario]); // ejecuta
+        $usuario = $sentencia->fetch(PDO::FETCH_OBJ); // obtiene la respuesta
+        return $usuario;
+    }
+
+    public function update($id_usuario, $tipo){
+        // 1. abro la conexión con MySQL 
+        $db = $this->createConection();
+        // 2. enviamos la consulta (3 pasos)
+        $sentencia = $db->prepare("UPDATE usuarios SET tipo = ? WHERE id_usuario = $id_usuario"); // prepara la consulta
+        $sentencia->execute([$tipo]); // ejecuta
     }
 }
